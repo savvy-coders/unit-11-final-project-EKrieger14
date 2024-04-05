83fcf989ca7ced5a03f4392aeb14c6ecabbd5631
Ethan Krieg <ethankrieg10@gmail.com>
2024-04-04 22:47:54 -0700

    Complete Final Project

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Add your script below this line, but above the next comment!
 
+function getBtnAndShow() {
+    function displayDateAndTime() {
+        document.getElementById('time').innerHTML = Date();
+    }
+    document
+    .getElementById('timeCheckBtn')
+    .addEventListener('click', displayDateAndTime);
+}
+
+function clearDateAndTime() {
+    document.getElementById('time').innerHTML = '';
+}
+
+getBtnAndShow();
+setInterval(clearDateAndTime, 5000);
+
 
 
 
